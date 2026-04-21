477a09c288ea8fbcf0180edbc7e981adf7603ce1
Aurélien <6292584+au2001@users.noreply.github.com>
2026-04-21 14:59:54 +0200

    Add archon.codesens.us

---
+++ well-known/src/codesens.us/archon/index.js
@@ -0,0 +1,1 @@
+export { default as "apple-app-site-association" } from "./apple-app-site-association.json";
--- well-known/src/well-known.js
+++ well-known/src/well-known.js
@@ -1,2 +1,3 @@
 export * as "arl.sh" from "./arl.sh/index.js";
 export * as "garnier.dev" from "./garnier.dev/index.js";
+export * as "archon.codesens.us" from "./codesens.us/archon/index.js";
