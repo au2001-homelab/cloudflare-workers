512dc515cba1d45e45268691be6ffe7753a23146
Aurélien <6292584+au2001@users.noreply.github.com>
2026-06-19 12:07:47 +0200

    Add no-cache header

--- well-known/src/worker.js
+++ well-known/src/worker.js
@@ -1,5 +1,9 @@
 import * as wellknown from "./well-known.js";
 
+const headers = {
+  "Cache-Control": "no-cache",
+};
+
 async function fetch(request) {
   const { hostname, pathname } = new URL(request.url);
 
@@ -7,11 +11,14 @@ async function fetch(request) {
     const data = wellknown[hostname][pathname.substring(13)];
     switch (typeof data) {
       case "string":
-        return new Response(data);
+        return new Response(data, {
+          headers,
+        });
 
       case "object":
         return new Response(JSON.stringify(data), {
           headers: {
+            ...headers,
             "Content-Type": "application/json",
           },
         });
